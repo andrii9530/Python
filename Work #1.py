18487c59c233480310e2f3380ae26b7150c2852c
andrii9530 <144731257+andrii9530@users.noreply.github.com>
2024-12-05 17:20:45 +0200

    Update Work #1.py
    Зробив неаелике покращення спрямоване на покращення комфорту при виконанні завдання

--- Work #1.py
+++ Work #1.py
@@ -1,50 +1,66 @@
+# Це для затримок в роботі*
+import time
+# Це щоб очищати термінал
+import os
+
+os.system('cls')
 
 # Перше та друге завдання
-# # Водимо назву фрукта
-# f = input("Ведіть фрукт клькіст якого бажаєте дізнатись ")
+print("-----Перше завдання у поєднанні з другим-----")
 
-# # Котедж з фруктами
-# Fruits = ("banana","cherry","Orange","cherry","Orange","bananac")
+# Водимо назву фрукта
+f = input("Ведіть фрукт клькіст якого бажаєте дізнатись: ")
 
-# # Лічильник
-# num = 0
+# Котедж з фруктами
+Fruits = ("banana","cherry","Orange","cherry","Orange","bananac")
 
-# for i in Fruits:
-#     if f == i or f in i:
-#         num += 1
-# print(num)
+# Лічильник
+num = 0
+
+for i in Fruits:
+    if f == i or f in i:
+        num += 1
+print(f"В списку {num} {f}")
+time.sleep(1)
+os.system('cls')
+
 
 # третє завдання
+print("----------------Третє завдання----------------")
+
 # Котедж з Автомобілями
-# Cars = ("Volkswagen","Tesla","Volkswagen","Toyota","Volkswagen","BMW")
+Cars = ("Volkswagen","Tesla","Volkswagen","Toyota","Volkswagen","BMW")
 
 
 
-# print("Всі доступні автомарки для заміни")
-# for i in Cars:
-#         print(i, "\t")
+print("Всі доступні автомарки для заміни")
+for i in Cars:
+        print(i, "\t")
 
-# car = input("Яку бажаєте змінити? ")
-# newCar = input("На яку бажаєте змінити? ")
-# Cars = list(Cars)
+car = input("Яку бажаєте змінити? ")
+newCar = input("На яку бажаєте змінити? ")
+Cars = list(Cars)
 
-# num = 0
-# for i in Cars:
-#     num += 1
-#     if i == car:
-#         Cars[num - 1] = newCar
+num = 0
+for i in Cars:
+    num += 1
+    if i == car:
+        Cars[num - 1] = newCar
 
-# Cars = tuple(Cars)
+Cars = tuple(Cars)
 
-# for i in Cars:
-#         print(i, "\t")
+for i in Cars:
+        print(i, "\t")
+time.sleep(3)
+os.system('cls')
 
 # Чeтверте завдання
-"""
+print("------------Четверте завдання--------------")
+
 numbers = (45, 578, 6643, 1234409857)
 num = 0
 for i in numbers:
         i = str(i)
         num += 1
         print(f"Цифра {num} Має {len(i)} елементи")
-"""
+
