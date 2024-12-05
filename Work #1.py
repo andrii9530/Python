90103daf793c73fb872478050dfff63786e0fb20
andrii9530 <144731257+andrii9530@users.noreply.github.com>
2024-12-05 15:03:39 +0200

    Add files via upload

---
+++ Work #1.py
@@ -0,0 +1,50 @@
+
+# Перше та друге завдання
+# # Водимо назву фрукта
+# f = input("Ведіть фрукт клькіст якого бажаєте дізнатись ")
+
+# # Котедж з фруктами
+# Fruits = ("banana","cherry","Orange","cherry","Orange","bananac")
+
+# # Лічильник
+# num = 0
+
+# for i in Fruits:
+#     if f == i or f in i:
+#         num += 1
+# print(num)
+
+# третє завдання
+# Котедж з Автомобілями
+# Cars = ("Volkswagen","Tesla","Volkswagen","Toyota","Volkswagen","BMW")
+
+
+
+# print("Всі доступні автомарки для заміни")
+# for i in Cars:
+#         print(i, "\t")
+
+# car = input("Яку бажаєте змінити? ")
+# newCar = input("На яку бажаєте змінити? ")
+# Cars = list(Cars)
+
+# num = 0
+# for i in Cars:
+#     num += 1
+#     if i == car:
+#         Cars[num - 1] = newCar
+
+# Cars = tuple(Cars)
+
+# for i in Cars:
+#         print(i, "\t")
+
+# Чeтверте завдання
+"""
+numbers = (45, 578, 6643, 1234409857)
+num = 0
+for i in numbers:
+        i = str(i)
+        num += 1
+        print(f"Цифра {num} Має {len(i)} елементи")
+"""
