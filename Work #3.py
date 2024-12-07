0a02cc110b3d593fea8bc5fc80ec53cc6a161f52
andrii9530 <144731257+andrii9530@users.noreply.github.com>
2024-12-07 21:03:09 +0200

    Update Work #3.py
    Змінив існуючі проблеми та покращив розуміння на що треба дивитись

--- Work #3.py
+++ Work #3.py
@@ -1,3 +1,5 @@
+# Класна робота
+"""
 citi1 = {"Рівне","Костопіль","Київ","Житомир","Львів"}
 citi2 = {"Рівне","Березне","Костопіль","Сарни","Житомир","Львів"}
 citi3 = citi2.intersection(citi1)
@@ -6,25 +8,26 @@ citi3 = citi1.difference(citi2)
 print(citi3)
 citi3 = citi2.difference(citi1)
 print(citi3)
-
+"""
+# Домашнє
 print("Четверте завдання")
 def getnumber(a,b):
     i = a
-    while i < b:
-        if i % 2 == 0 and i >= a and i <= b:
+    while i <= b:
+        if i % 2 != 0:
             yield i
         i += 1
-for i in getnumber(2,9):
-    print(i)
+
+for num in getnumber(2,9):
+    print(num)
 
 
 print("П'яте завдання")
 number = [3,4,5,8,1,35,578,234,4]
 def getnumber(a,b, num):
     for i in num:
-        if i > a and i < b:
+        if i < a or i > b:
             yield i
-        i += 1
 
 for i in getnumber(2,9,number):
-    print(i)       
+    print(i)     
