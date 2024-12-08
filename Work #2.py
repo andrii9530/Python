cd4f54aff16c6f03baf1a756b55941ef92f60edd
andrii9530 <144731257+andrii9530@users.noreply.github.com>
2024-12-09 01:00:15 +0200

    Update Work #2.py
    Додав другу частину домашнього завдання

--- Work #2.py
+++ Work #2.py
@@ -45,3 +45,103 @@ def printN(*args):
     return 0
 
 printN("Привіт",2,6,-2,7,-1,0)
+
+# Друга домашка за цю пару
+os.system('cls')
+thisdict = {}
+# Меню
+name = ""
+password = ""
+num = 1
+text = ""
+while num != 0:
+    num = int(input("\tМеню\n1)Додати нового користувача\n2)Видали користувача\n3)Змінити пароль\n4)Перевірити пароль\n5)Отримати пароль (за логіном)\n6)Зберегти в файл\n0)Вийти\nВиберіть дію: "))
+    if num == 1:
+        # Додати нового користувача
+        name = input("Ведіть логін: ")
+        if len(thisdict) != 0:
+            if name in thisdict:
+                print("Такий користувач уже існує")
+            else:
+                password = input("Ведіть пароль: ")
+                thisdict[name] = password
+                print("Користувача додано")
+                time.sleep(1)
+        else:
+            password = input("Ведіть пароль: ")
+            thisdict[name] = password
+            print("Користувача додано")
+            time.sleep(1)
+        os.system('cls')
+    elif num == 2:
+        # Видали користувача
+        if len(thisdict) != 0:
+            name = input("Ведіть логін користувача якого хочете видалити: ")
+            if name in thisdict:
+                thisdict.pop(name)
+                print(f"Користувача{name} успішно було видаленно")
+            else:
+                print("Такого корисутача немає")
+
+        else:
+            print("Ще не було доданого користувача і в звязку з цим немає кого видаляти")
+        time.sleep(3)
+        os.system('cls')
+    elif num == 3:
+        # Змінити пароль (Має бути захищеним тобто не манше 6 символів*)
+        if len(thisdict) != 0:
+            name = input("Ведіть голін: ")
+            if name in thisdict:
+                password = input("Ведіть новий пароль: ")
+                if thisdict[name] != password:
+                    thisdict[name] = password
+                    print("Пароль успішно змінено")
+                else:
+                    print("Паролі одинакові, спроба змінити пароль не є успішною")
+                    time.sleep(2)
+            else:
+                print("Такого логіну немає")
+                time.sleep(1)
+        else:
+            print("Ще не було доданого користувача і в звязку з цим не можна змінити пароль")
+        time.sleep(3)
+        os.system('cls')
+    elif num == 4:
+        # Перевірити паролі
+        n = 0
+        if len(thisdict) != 0:
+            for key,password  in thisdict.items():
+                if len(password) < 6:
+                    print(f"Пароль {password} є незахищеним ")
+                else:
+                    n += 1
+            if n == len(thisdict):
+                print("Всі паролі є захищеними")
+            time.sleep(1)
+        else:
+            print("Ще не було доданого користувача і в звязку з цим немає що редагувати")
+        time.sleep(3)
+        os.system('cls')
+    elif num == 5:
+        # Отримати пароль
+        if len(thisdict) != 0:
+            name = input("Ведіть логін користувача щоб переглянути пароль: ")
+            if name in thisdict:
+                print(f"Ваш пароль {thisdict[name]}")
+            else:
+                print("Інформацію не вдалося отримати, дані не є коректними")
+            time.sleep(3)
+        else:
+            print("Ще не було доданого користувача і в звязку з цим немає що редагувати")
+        time.sleep(3)
+        os.system('cls')
+    elif num == 6:
+        # Зберегти в файл
+        with open("text.txt", "a") as t:
+            for key,password  in thisdict.items():
+                t.write(f"{key}: {password}\n")
+        print("Дані успішно збережено в файл text.txt")
+        time.sleep(2)
+        os.system('cls')
+    else:
+        print("Вихід успішний")
