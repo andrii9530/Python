f7303dc216bc26bfd77f72ef757ca3812afe0ed5
andrii9530 <144731257+andrii9530@users.noreply.github.com>
2025-01-18 17:26:08 +0200

    Update views.py

--- film_project/films/views.py
+++ film_project/films/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Film
 # Create your views here
 
 def film_list(reguest):
     films = Film.objects.all()
     return render(reguest, 'films/film_list.html', {'films':films})
+
+def film_detail(reguest,pk):
+    film = get_object_or_404(Film, pk=pk)
+    return render(reguest, 'films/film_detail.html', {'film':film})
