396c650786a023d6de1bb696733c64ecd1c1ceba
andrii9530 <144731257+andrii9530@users.noreply.github.com>
2025-01-22 15:50:15 +0200

    Update views.py

--- film_project/films/views.py
+++ film_project/films/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Film
-# Create your views here
+from .models import Film, Genre
 
-def film_list(reguest):
-    films = Film.objects.all()
-    return render(reguest, 'films/film_list.html', {'films':films})
+def film_list(request):
+    genre_filter = request.GET.get('genre', '')  
+    genres = Genre.objects.all()  
+    films = Film.objects.all()  
+    
+    if genre_filter:
+        films = films.filter(genres__id=genre_filter)
+
+    return render(request, 'films/film_list.html', {'films': films, 'genres': genres, 'request': request})
+
+
 
 def film_detail(reguest,pk):
     film = get_object_or_404(Film, pk=pk)
     return render(reguest, 'films/film_detail.html', {'film':film})
+    
