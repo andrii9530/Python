3defbb9710764f9dd589af28cbe38a6e0863273f
andrii9530 <144731257+andrii9530@users.noreply.github.com>
2025-01-24 00:13:15 +0200

    Update urls.py
    Додав можливості для адміна але ще треба буде доробити функціонал та вигляд

--- film_project/films/urls.py
+++ film_project/films/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.film_list, name='film_list'),
     path('<int:pk>/', views.film_detail, name='film_detail'),
+    path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('add-film/', views.add_film, name='add_film'),
+    path('edit-film/<int:film_id>/', views.edit_film, name='edit_film'),
+    path('delete-film/<int:film_id>/', views.delete_film, name='delete_film'),
+
 ]
